e1f0b60509ba43e4b089af83646d0766e58687b1
Luciano Matos <luciano7matos@gmail.com>
2021-05-31 20:15:36 -0300

    Add files via upload

---
+++ Python/listas.py
@@ -0,0 +1,7 @@
+valores = []
+valores.append(2)
+valores.append(10)
+valores.append(15)
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('Cheguei ao final da lista')
